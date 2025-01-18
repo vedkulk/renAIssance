219bad6a40b911898783edc03de1fbf36e3efaa7
Jinesh <jineshnanal04@gmail.com>
2025-01-18 11:30:34 +0530

    Changes

--- Backend/app.py
+++ Backend/app.py
@@ -14,23 +14,24 @@ load_dotenv() #load the env variables (Api key)
 app = Flask(__name__)
 CORS(app)
 
-WORQHAT_API_KEY=os.getenv("WORQHAT_API_KEY")
+WORQHAT_API_KEY = os.getenv("WORQHAT_API_KEY")
+
 
 def speech_to_text(audio_path):
     with open(audio_path, "rb") as audio_file:
         response=requests.post(
-            "https://api.worqhat.com/api/ai/content/v4", #Multimodal Input Conversational
+            "https://api.worqhat.com/api/ai/speech-text", #Multimodal Input Conversational
             headers={"Authorization": f"Bearer {WORQHAT_API_KEY}"},
             files={"file": audio_file},
             data={
                 "question":"Transribe this audio",
-                "model":"aion-v4-nano-160824"
+                "model":"Speech Text Extraction"
             },
         )
-    return response.json().get("text","Speech-To-text conversion failed")
+    print("Full JSON Response:", response.json())
 
 def main():
-    audio_path='xyz'
+    audio_path='example.mp3'
     transcription = speech_to_text(audio_path)
     print("Transcription : ", transcription)
 
